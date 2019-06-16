c7d2d7cdac3205dd1cea35b35cb9c6ce80f7c6f9
Nguyen Van Lan <lannguyenit598@gmail.com>
2019-06-16 14:44:12 +0700

    sửa đổi file app.js (tổ chức lại theo mô hình mvc)

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var moongose = require('mongoose');
+
+
+
 var index = require('./routes/index');
-var admin = require('./routes/admin');
+var cartRouter = require('./routes/cart');
+var checkoutRouter = require('./routes/checkout');
+var accountRouter = require('./routes/account');
+var orderRouter = require('./routes/order');
+var productRouter = require('./routes/product');
+
 var app = express();
 
-
+moongose.connect('mongodb://localhost:27017/bookshop',{useNewUrlParser: true});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -14,10 +23,20 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+
 // routes
 app.use('/', index);
-app.use('/admin', admin);
+app.use('/cart', cartRouter);
+app.use('/checkout', checkoutRouter);
+app.use('/account_setting', accountRouter);
+app.use('/my-order', orderRouter);
+app.use('/shop',productRouter);
 
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true })); 
+// app.use(multer());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
